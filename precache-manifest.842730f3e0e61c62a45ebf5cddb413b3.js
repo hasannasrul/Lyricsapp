b4624286584001686bba4a2f9ea5860c63b71366
nasrul <hasanali242424@gmail.com>
2020-09-01 18:43:45 +0530

    Updates

---
+++ precache-manifest.842730f3e0e61c62a45ebf5cddb413b3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "532c5d09a8e99f87f07a996150147907",
+    "url": "/lyricsapp/index.html"
+  },
+  {
+    "revision": "e3ca8a6bac9538ee6498",
+    "url": "/lyricsapp/static/css/main.bbf4c102.chunk.css"
+  },
+  {
+    "revision": "231c1d715b539e80d2c8",
+    "url": "/lyricsapp/static/js/2.efb3460d.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/lyricsapp/static/js/2.efb3460d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e3ca8a6bac9538ee6498",
+    "url": "/lyricsapp/static/js/main.09f1442c.chunk.js"
+  },
+  {
+    "revision": "9c977f6e9d0537289972",
+    "url": "/lyricsapp/static/js/runtime-main.14932e8c.js"
+  }
+]);
